202f4883d22ee1cc81562704f93403e93913a7cc
Mark <mark@markrabey.com>
2017-08-09 14:19:12 -0400

    added basic docs and webpack configs

---
+++ docs/index.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.scss';
+
+const Demo = () => (
+  <div className="demo">
+    Breakout Demo
+  </div>
+);
+
+ReactDOM.render(<Demo />, document.getElementById('content'));
